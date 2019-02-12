e1c4fb4f36a9d5cdf26ce51dd860079162f3b5a9
Gabriel Cadiz <gabecadiz@hotmail.com>
2019-02-12 00:22:28 +0000

    refactored download_avatars.js, removed some stream response whose purpose was to console log statuses

--- download_avatars.js
+++ download_avatars.js
@@ -2,6 +2,9 @@ var request = require('request');
 var secret = require('./secret.js');
 var fs = require('fs');
 
+// execute program in command line like this:
+// node download_avatars.js <owner> <repo>
+
 const gitHubUser = process.argv.slice(2)[0];
 const gitHubRepository = process.argv.slice(2)[1];
 
@@ -12,7 +15,6 @@ function getRepoContributors(repoOwner, repoName, cb) {
   if(repoOwner === undefined || repoName === undefined){
     return console.log("Error!!! \nBoth repo owner and repo name arguments are required in order to run the program");
   }
-
   var options = {
     url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
     headers: {
@@ -22,16 +24,6 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
   request(options, function(err,res,body){
     cb(err,body);
-      var info = JSON.parse(body);
-
-      //gets each user's avatar url
-      info.forEach(function(eachUser){
-        var avatarUrl = eachUser.avatar_url;
-        //creates filepath for coming download
-        var imagePath = `avatars/${eachUser.login}.jpg`;
-
-        downloadImageByURL(avatarUrl, imagePath);
-      });
   });
 }
 
@@ -41,25 +33,20 @@ function downloadImageByURL(url, filePath) {
        .on('error', function(err) {
          console.log("error occured:", err);
        })
-       .on('response', function(response) {
-         console.log('Response Status Code: ', response.statusCode);
-         console.log(response.headers['content-type']);
-
-       })
-       .on('data', function(data){
-          console.log("Downloading image...");
-       })
        .pipe(fs.createWriteStream(filePath))
        .on('finish',function(){
-          console.log("Download Complete");
+          console.log("Image Download Complete");
        });
 }
 
 getRepoContributors(gitHubUser, gitHubRepository, function(err, result) {
-
   console.log("Errors:", err);
-  //console.log("Result:", result);
+        var info = JSON.parse(result);
+      //gets each user's avatar url
+        info.forEach(function(eachUser){
+          var avatarUrl = eachUser.avatar_url;
+        //creates filepath for coming download
+          var imagePath = `avatars/${eachUser.login}.jpg`;
+          downloadImageByURL(avatarUrl, imagePath);
+        });
 });
-
-
-// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
